b196368b400a58d76a844e098b4ae0dbab6c094c
Wobbly74 <42060159+Wobbly74@users.noreply.github.com>
2022-01-05 15:33:32 +0000

    Updated setup info

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup
 
 setup(name='cbpi4-MashHoldStep',
       version='0.0.1',
-      description='CraftBeerPi Plugin',
+      description='CraftBeerPi Plugin to allow resetting target temp at end of mash step to be configurable',
       author='Wobbly',
       author_email='neil@milne.com',
-      url='',
+      url='https://github.com/Wobbly74/cbpi4-MashHoldStep',
+      license='GPLv3',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi4-MashHoldStep': ['*','*.txt', '*.rst', '*.yaml']},
       packages=['cbpi4-MashHoldStep'],
-     )
+           install_requires=[
+            'cbpi',
+      ]
+)
